574b795dd1ce841c8f18e54bdc8073cd2f43b5f5
QA <qasmendoza@gmail.com>
2023-09-13 02:42:18 -0500

    update data

--- Data.py
+++ Data.py
@@ -5,35 +5,40 @@ import numpy as np
 df = pd.read_csv('crop_recommendation_orig.csv')
 
 # Define the function to augment data
-def augment_data(subset, n_times):
-    augmented = subset.copy()
+def augment_data(subset, n_times, noise_scale=0.10):
+    augmented_list = [subset]
+
     for _ in range(n_times-1):
-        noise = np.random.normal(loc=0, scale=0.10, size=subset.shape)
+        noise = np.random.normal(loc=0, scale=noise_scale, size=subset.shape)
         new_data = subset.copy()
 
         # Alter numerical columns
         for col in ['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall']:
             if col in ['N', 'P', 'K']:
-                # Round the values for these columns after adding noise
                 new_data[col] = np.round(new_data[col] + noise[:, list(subset.columns).index(col)])
+                # Ensure values are not negative
+                new_data[col] = new_data[col].clip(0)
             else:
                 new_data[col] += noise[:, list(subset.columns).index(col)]
         
-        augmented = pd.concat([augmented, new_data])
-    return augmented
+        augmented_list.append(new_data)
+
+    return pd.concat(augmented_list)
 
 # Unique labels in the dataset
 labels = df['label'].unique()
 
-# Create an empty dataframe to hold augmented data
-augmented_df = pd.DataFrame()
+# List to hold augmented data subsets
+augmented_data_list = []
 
 # Loop through labels and apply augmentation
 for label in labels:
     subset = df[df['label'] == label]
-    augmented_subset = augment_data(subset, 20) # x20 the data
-    augmented_df = pd.concat([augmented_df, augmented_subset])
+    augmented_subset = augment_data(subset, 20)
+    augmented_data_list.append(augmented_subset)
+
+# Concatenate all augmented subsets
+augmented_df = pd.concat(augmented_data_list)
 
 # Save augmented dataframe to a new csv
 augmented_df.to_csv('augmented_file.csv', index=False)
-
